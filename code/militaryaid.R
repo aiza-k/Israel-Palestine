31e7e8487c2639d10b548e7f6a02829cf28f2943
aiza-k <aizakabeer@gmail.com>
2017-11-19 17:55:36 -0500

    update deaths code

--- code/militaryaid.R
+++ code/militaryaid.R
@@ -20,16 +20,40 @@ table(Israel$`Funding Account Name`)
 Israel_AidByYear<-aggregate(Israel[8:9], by=list('Fiscal Year'=Israel$'Fiscal Year'), FUN=sum)
 
 #Miliary aid in Constant Dollars vs Fiscal Year
+
+#Min aid in 1961 of 6.227e+03, max in 1979 of 1.099e+10, mean of 2.635e+09.
+summary(Israel_AidByYear$ConstantDollars)
+which.max(Israel_AidByYear$ConstantDollars)
+which.min(Israel_AidByYear$ConstantDollars)
+#plot is moderately strong and positive with some outliers
 plot(ConstantDollars ~ `Fiscal Year`, Israel_AidByYear)
+#The slope is 3.941e+07. 
+#This implies that for every 1 year increase in time, the predicted total military aid to Israel willincreases by 3.941e+07 dollars.
 model <- lm(ConstantDollars ~ `Fiscal Year`, Israel_AidByYear)
 abline(model, col = 'blue')
 model
+#outliers:
+#there are outliers in the years 1974, 1976, and 1979
+Outliers_CD <- Israel_AidByYear[Israel_AidByYear$ConstantDollars > 4e+09,] 
+View(Outliers_CD)
+
 
 #Miliary aid in Historical Dollars vs Fiscal Year
+
+#Min aid in 1961 of 1.000e+03, max in 1979 of 4.000e+09, mean of 1.574e+09
+summary(Israel_AidByYear$HistoricalDollars)
+which.max(Israel_AidByYear$HistoricalDollars)
+which.min(Israel_AidByYear$HistoricalDollars)
+#plot is strong and positive with some outliers
 plot(HistoricalDollars ~ `Fiscal Year`, Israel_AidByYear)
+#The slope is 3.941e+07. 
+#This implies that for every 1 year increase in time, the predicted total military aid to Israel willincreases by 3.941e+07 dollars.
 model2 <- lm(HistoricalDollars ~ `Fiscal Year`, Israel_AidByYear)
 abline(model2, col = 'blue')
 model2
+#outliers:
+#there are outliers in the years 1974 and 1979
 
-
+#Try converting outliers to missing, then find max, min, median and model
 #check USmil aid and deaths in and around 2000-2005, 2008, and 2014
+#also check what, if anything, happened in outlier years to generate more aid?
